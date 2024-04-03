22064a676e85555a60502c99ee5007e738abada6
1Kr3ksu <kacperbielawski33@gmail.com>
2024-04-03 13:23:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=stylesheet href=style.css>
+    <title>Sklep</title>
+</head>
+
+<body>
+    <header class="flex-container topnav">
+        <img src="img/image-removebg-preview.png" alt="logo" id="logo">
+        <form action="search.php" method="get">
+            <input type="text" name="search" id="search" placeholder="Wyszukaj Produkt">
+            <input type="submit" value="Szukaj">
+        </form>
+        <a href="cart.php" id="cart">Koszyk</a>
+    </header>
+
+
+    <main class="flex-container-wrap">
+        <div class="card">
+            <h3>nazwa oferty </h3>
+            <!--cena-->
+            <p>cena:100zł</p>
+        </div>
+
+    </main>
+</body>
+
+</html>
